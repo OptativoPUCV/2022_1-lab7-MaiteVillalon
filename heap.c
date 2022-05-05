64887d2a75a5e0ad6c98bcb944cc42819a126fb3
MaiteVillalon1 <>
2022-05-05 16:07:49 +0000

    new try

--- heap.c
+++ heap.c
@@ -39,7 +39,8 @@ void heap_push(Heap* pq, void* data, int priority)
 
   if (pq->size == pq->capac)
   {
-    pq = realloc(pq, (pq->capac *2) + 1);
+    pq->capac = (pq->capac * 2 ) + 1;
+    pq->heapArray = (heapElem*) realloc(pq->heapArray, (pq->capac)*sizeof(heapElem);
   }
 
   pq->heapArray[pq->size].data = data;
